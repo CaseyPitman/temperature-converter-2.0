28670dbaabed1a2988f6fa78783dc0ab17d7fba2
Casey The Great <casey@caseypitman.com>
2020-07-20 16:01:47 -0500

    Mapped out js flow

--- app.js
+++ app.js
@@ -1,72 +1,109 @@
 /*********** Temperature Conversion App ***********/
 
+/**********
+*   Data  *
+***********/
 
-let state = { };
 
+const dataController = (() => { 
+   let tempResults = {};
+
+   return {
+      //tempResults: { },    possibly put this here.
+
+      calculateTemp: (temp, scale) => {
+
+         if (scale === 'farenheit'){
+            tempResults.farenheit = temp;
+            tempResults.celsius = ((temp - 32) * (5 / 9));
+            tempResults.kelvin = (((temp - 32) * (5 / 9)) + 273.15);
+         } else if ( scale === 'celsius'){
+            tempResults.farenheit = ((temp * 1.8) + 32);
+            tempResults.celsius = temp;
+            tempResults.kelvin = temp + 273.15;
+         } else if ( scale === 'kelvin'){
+            tempResults.farenheit = (((temp - 273.15) * 1.8) + 32);
+            tempResults.celsius = temp - 273.15;
+            tempResults.kelvin = temp;
+         }
+      },
+
+      getResults: () => {
+         return tempResults;
+      }
+
+   }
+})();
 
 
 /***************
 * UI Controller*
 ****************/
 
-//MAKE EACH MODULE A FUNCTOIN THAT RETURNS AN OBJECT FULL OF FUNCTIONS
+const uiController = (() => {
+
+   //DOMstrings
+   const elements = {
+      header: document.getElementById('header'),
+      tempInput: document.getElementById('input-temp'),
+      results: document.getElementById('result-div'),
+      farenheitResult: document.getElementById('farenheit-result'),
+      celsiusResult: document.getElementById('celsius-result'),
+      kelivinResult: document.getElementById('kelvin-result')
+   };
 
 
-//DOMstrings
-const elements = {
-   header: document.getElementById('header'),
-   tempInput: document.getElementById('input-temp'),
-   results: document.getElementById('result-div'),
-   farenheitResult: document.getElementById('farenheit-result'),
-   celsiusResult: document.getElementById('celsius-result'),
-   kelivinResult: document.getElementById('kelvin-result')
-}
+   return {
+   //Determines date and sets the header image based on season of the year. 
+      setBackgroundImg : () => {
+
+         let month, season, imgSrc;
+         month = new Date().getMonth();
+
+         //Determine which season it is.
+         if (month > 1 && month< 5){  //Spring - MAR, APR, MAY
+            season = `spring`;
+         } else if (month > 4 && month< 8) {  //Summer - JUNE, JULY, AUG
+            season = `summer`;
+         } else if (month > 7 && month < 11) {   //Fall - SEPT, OCT, NOV
+            season = `fall`;
+         } else {                               //Winter - DEC, JAN, FEB
+            season = `winter`;
+         }
+
+         //Set the background based on the season.
+         elements.header.insertAdjacentHTML('beforeend', `<img src="img/${season}-crop.jpg" alt="${season} image" id="header-img">`)
+      },
+
+      clearInput: () => {
+         //Resets input on init
+         //Resets selector
+
+      },
 
 
-//Determines date and sets the header image based on season of the year. 
-const setBackgroundImg = () => {
+      getInput: () => {
+         //Get input from DOM.   - ensure proper formatting
 
-   let month, season, imgSrc;
-   month = new Date().getMonth();
 
-   //Determine which season it is.
-   if (month > 1 && month< 5){  //Spring - MAR, APR, MAY
-      season = `spring`;
-   } else if (month > 4 && month< 8) {  //Summer - JUNE, JULY, AUG
-      season = `summer`;
-   } else if (month > 7 && month < 11) {   //Fall - SEPT, OCT, NOV
-      season = `fall`;
-   } else {                               //Winter - DEC, JAN, FEB
-      season = `winter`;
+         //Get input type from DOM;  - ensure proper formatting
+
+         //return inputs as object
+
+
+      },
+
+      displayResults: (tempResults) => {
+
+
+      },
+
+      getElements: function(){
+         return elements;
+      }
+
    }
-
-   //Set the background based on the season.
-   elements.header.insertAdjacentHTML('beforeend', `<img src="img/${season}.jpg" alt="${season} image" class="header-img">`)
-   
-   
-}
-
-//Gets input temperature from UI
-
-//Gets input type from UI
-
-//Displays results on UI
-
-
-
-
-/**********
-*   Data  *
-***********/
-
-//Calculates converted temperatures
-
-//Calculates Farenheit
-
-//Calculates Celsius
-
-//Calculates Kelvin
-
+})();
 
 
 
@@ -74,12 +111,38 @@ const setBackgroundImg = () => {
 * Controller *
 **************/
 
+const controller = ((data, ui) => {
+   //Get DOMstrings elements
 
-const init = () => {
-   setBackgroundImg();
-}
+   //Set up event listeners
 
-init();
+   //Calls for getting input and type
+
+   //Calls for making the calculations
+
+   //Calls for displaying the calculations
+      //pass the temp results object
+
+
+   //Return init
+   return {
+      init: () => {
+         ui.setBackgroundImg();
+         //Clear inputs
+         //Reset scale selector
+         //Clear results
+
+      }
+   }
+
+})(dataController, uiController);
+
+
+controller.init();
+
+
+
+
 
 //init function 
    //clears inputs
